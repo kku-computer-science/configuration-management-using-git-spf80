d3edb47acf1c7b2d8db109065e31956432aa7e9e
Kimmypim <pimapsorn.p@kkumail.com>
2025-12-10 16:25:33 +0700

    Update output

---
+++ Project/TeamProject.java
@@ -0,0 +1,45 @@
+package Project;
+
+import java.util.Scanner;
+
+public class TeamProject {
+    public static void main(String[] args) {
+        // 1.Input and before sorting
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.print("Enter " + n + " numbers : ");
+        for(int i=0 ; i<n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Before sorting : ");
+        for(int i=0 ; i<n ; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        // 2.bubble sorting
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                // เปรียบเทียบตัวที่อยู่ติดกัน ถ้าตัวหน้ามากกว่าตัวหลัง ให้สลับที่
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // 3.Output after sorting
+        System.out.println("--Result--");
+        System.out.print("After sorting : ");
+        for(int i = 0; i < n; i++) {  //เริ่มวนลูป index
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
